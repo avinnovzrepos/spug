58759dabac2d7ef80c9240137bf314bff3d5e604
vicruz <virmelcruz@gmail.com>
2016-06-18 00:37:35 +0800

    finished form for electrical and mechanical spare parts

--- client/app/item/item.controller.js
+++ client/app/item/item.controller.js
@@ -2,15 +2,229 @@
 (function(){
 
 class ItemComponent {
-  constructor() {
+  constructor($state) {
     this.message = 'Hello';
+    this.itemType = $state.params.itemType === 'mechanical' ? true : false;
+    this.capacities = [];
+    this.brands = [];
+    this.compNos = [];
+    this.categories = [];
+    this.elecTypes = [];
+    this.facilities = [];
+    this.facilityTypes = [];
+    this.outVoltages = [];
+  }
+
+  $onInit() {
+    var initialNum = this.itemType ? '0000' : '00';
+    for (var i = 1; i <= 8; i++) {
+      this.compNos.push(initialNum+i);
+    };
+    console.log(this.itemType);
+    if(this.itemType) {
+      this.categories.push(
+        {
+          label: 'BASIC ENGINE - CAMSHAFT',
+          value: '001'
+        },
+        {
+          label: 'BASIC ENGINE - CONNECTING ROD',
+          value: '002'
+        },
+        {
+          label: 'BASIC ENGINE - CRANKSHAFT',
+          value: '003'
+        },
+        {
+          label: 'BASIC ENGINE - ENGINE BLOCK',
+          value: '004'
+        },
+        {
+          label: 'BASIC ENGINE - CYLINDER HEAD',
+          value: '005'
+        }
+      );
+      this.brands.push(
+        {
+          label: 'CATERPILLAR',
+          value: '01'
+        },
+        {
+          label: 'CKD',
+          value: '02'
+        },
+        {
+          label: 'CUMMINS',
+          value: '03'
+        },
+        {
+          label: 'CUMMINS-STANFORD',
+          value: '04'
+        },
+        {
+          label: 'CUMPER',
+          value: '05'
+        }
+      );
+      this.capacities.push(
+        {
+          label: '6 KW (ENGINE CAPACITY)',
+          value: '01'
+        },
+        {
+          label: '12 KW (ENGINE CAPACITY)',
+          value: '02'
+        },
+        {
+          label: '15 KW (ENGINE CAPACITY)',
+          value: '03'
+        },
+        {
+          label: '20 KW (ENGINE CAPACITY)',
+          value: '04'
+        },
+        {
+          label: '22 KW (ENGINE CAPACITY)',
+          value: '05'
+        }
+      );
+    } else {
+      this.categories.push(
+        {
+          label: 'Engine',
+          value: '001'
+        },
+        {
+          label: 'Turbine',
+          value: '002'
+        },
+        {
+          label: 'PV Panel',
+          value: '003'
+        },
+        {
+          label: 'Generator',
+          value: '004'
+        },
+        {
+          label: 'Plant Control Panel',
+          value: '005'
+        }
+      );
+      this.elecTypes.push(
+        {
+          label: 'Equipment',
+          value: '1'
+        },
+        {
+          label: 'Device',
+          value: '2'
+        },
+        {
+          label: 'Hardware',
+          value: '3'
+        },
+        {
+          label: 'Wires',
+          value: '4'
+        },
+        {
+          label: 'Poles',
+          value: '5'
+        },
+        {
+          label: 'Others',
+          value: '6'
+        }
+      );
+      this.facilities.push(
+        {
+          label: 'Plant',
+          value: '01'
+        },
+        {
+          label: 'Substation',
+          value: '02'
+        },
+        {
+          label: 'Lines',
+          value: '03'
+        }
+      );
+      this.facilityTypes.push(
+        {
+          label: 'Diesel',
+          value: '01'
+        },
+        {
+          label: 'Hydro',
+          value: '02'
+        },
+        {
+          label: 'Wind',
+          value: '03'
+        },
+        {
+          label: 'Solar',
+          value: '04'
+        },
+        {
+          label: 'Substation',
+          value: '05'
+        }
+      );
+      this.outVoltages.push(
+        {
+          label: '0V',
+          value: '01'
+        },
+        {
+          label: '240V',
+          value: '02'
+        },
+        {
+          label: '480V',
+          value: '03'
+        },
+        {
+          label: '4160V',
+          value: '04'
+        },
+        {
+          label: '13.8k V',
+          value: '05'
+        },
+        {
+          label: '69k V',
+          value: '06'
+        },
+        {
+          label: '138k V',
+          value: '07'
+        },
+        {
+          label: '230k V',
+          value: '08'
+        }
+      );
+    }
+  }
+
+  submit(item) {
+    if(this.itemType) {
+      item.stockCode = item.mechanical + item.brand.value + item.capacity.value + item.mechSpares + item.num7th + item.category.value + item.compNo;
+    } else {
+      item.stockCode = item.electrical + item.facility.value + item.facilityType.value + item.num6th + item.elecType.value + item.outVoltage.value + item.category.value + item.compNo;
+    }
+    console.log(item);
   }
 }
 
 angular.module('spugApp')
   .component('item', {
     templateUrl: 'app/item/item.html',
-    controller: ItemComponent
+    controller: ItemComponent,
+    controllerAs: 'itemStock'
   });
 
 })();
