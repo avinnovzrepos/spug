390569256c388d092df0c4213e2d43645dcc19f5
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-06-18 00:32:41 +0800

    Items Endpoint

---
+++ server/api/item/item.model.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+var ItemSchema = new mongoose.Schema({
+  code: String,
+  name: String,
+  partNumber: String,
+  specification: String,
+  unitOfMeasurement: String,
+  unitCost: Number,
+  year: String,
+  categoryId: String,
+  componentId: String,
+  other: Number,
+
+  // Mechanical Fields
+  mechanical: String,
+  brand: String,
+  capacity: String,
+  mechanicalSpares: String,
+
+  // Electrical Fields
+  electrical: String,
+  facility: String,
+  electricalType: String,
+  facilityType: String,
+  outputVoltage: String,
+
+  active: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+export default mongoose.model('Item', ItemSchema);
