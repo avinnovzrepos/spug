0471e9b16153c48ce672700ba03b7583efd5428b
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-06-27 21:46:51 +0800

    Update measurement unit model

--- server/api/measurement-unit/index.js
+++ server/api/measurement-unit/index.js
@@ -8,9 +8,9 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.post('/', auth.hasRole('super-admin'), controller.create);
-router.put('/:id', auth.hasRole('super-admin'), controller.update);
-router.patch('/:id', auth.hasRole('super-admin'), controller.update);
-router.delete('/:id', auth.hasRole('super-admin'), controller.destroy);
+router.post('/', auth.hasRole('superadmin'), controller.create);
+router.put('/:id', auth.hasRole('superadmin'), controller.update);
+router.patch('/:id', auth.hasRole('superadmin'), controller.update);
+router.delete('/:id', auth.hasRole('superadmin'), controller.destroy);
 
 module.exports = router;
--- server/api/measurement-unit/measurement-unit.model.js
+++ server/api/measurement-unit/measurement-unit.model.js
@@ -9,6 +9,32 @@ var MeasurementUnitSchema = new mongoose.Schema({
     type: Boolean,
     default: true
   }
+}, {
+  timestamps: true
 });
+
+
+// Validate duplicate name
+MeasurementUnitSchema
+  .path('name')
+  .validate(function(name, respond) {
+    var self = this;
+    return this.constructor.findOne({ name: name }).exec()
+      .then(function(measurementUnit) {
+        if (measurementUnit) {
+          if (self.id === measurementUnit.id) {
+            return respond(true);
+          }
+          if (!measurementUnit.active) {
+            return respond(true);
+          }
+          return respond(false);
+        }
+        return respond(true);
+      })
+      .catch(function(err) {
+        throw err;
+      });
+  }, 'Cannot have duplicate measurement unit name');
 
 export default mongoose.model('MeasurementUnit', MeasurementUnitSchema);
