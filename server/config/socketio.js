26f446f5f9f98f34eb658075d74bfdfe9e2dfea1
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-06-29 23:35:38 +0800

    Adjust socket connections listener limit

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -42,6 +42,8 @@ export default function(socketio) {
   // }));
 
   socketio.on('connection', function(socket) {
+    socket.setMaxListeners(20);
+
     socket.address = socket.request.connection.remoteAddress +
       ':' + socket.request.connection.remotePort;
 
