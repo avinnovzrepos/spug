2c518a2839d6c2219f94c5a6f1b1df93faeeee39
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2016-07-01 23:34:19 +0800

    Update endpoints authentication

--- server/api/inventory/index.js
+++ server/api/inventory/index.js
@@ -7,8 +7,8 @@ import * as auth from '../../auth/auth.service';
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', auth.hasRole('superadmin'), controller.create);
 router.put('/:id', auth.hasRole('superadmin'), controller.update);
 router.patch('/:id', auth.hasRole('superadmin'), controller.update);
